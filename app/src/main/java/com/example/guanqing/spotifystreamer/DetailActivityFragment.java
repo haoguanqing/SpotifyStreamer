242fd91659dd0d903e1ad55735596c49ec15e576
Guanqing <haoguanqing@live.com>
2015-06-23 04:19:39 -0400

    configure Library & create UI

--- app/src/main/java/com/example/guanqing/spotifystreamer/DetailActivityFragment.java
+++ app/src/main/java/com/example/guanqing/spotifystreamer/DetailActivityFragment.java
@@ -10,14 +10,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class DetailActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public DetailActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_search, container, false);
+        return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 }
