359f7b84fdcfbcd11fd97c7bafa666eaf14e9784
Guanqing <haoguanqing@live.com>
2015-07-16 18:41:31 -0400

    show dialog

--- app/src/main/java/com/example/guanqing/spotifystreamer/playTrack/PlayTrackFragment.java
+++ app/src/main/java/com/example/guanqing/spotifystreamer/playTrack/PlayTrackFragment.java
@@ -1,25 +1,21 @@
 package com.example.guanqing.spotifystreamer.playTrack;
 
 import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.RelativeLayout;
 
 import com.example.guanqing.spotifystreamer.R;
-
-import kaaes.spotify.webapi.android.models.Track;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlayTrackFragment extends android.support.v4.app.DialogFragment {
-    final static String PARCEL_KEY = "TRACK_PARCEL_KEY";
+    public final static String PARCEL_KEY = "TRACK_PARCEL_KEY";
+    public final static String TRACK_ID_KEY = "TRACK_ID_KEY";
 
     public PlayTrackFragment() {
     }
@@ -28,35 +24,21 @@ public class PlayTrackFragment extends android.support.v4.app.DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_play_track, container, false);
-
         return rootView;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        Dialog dialog = super.onCreateDialog(savedInstanceState);
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        return dialog;
-
-        // the content
-        final RelativeLayout root = new RelativeLayout(getActivity());
-        root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-
-        // creating the fullscreen dialog
-        final Dialog dialog = new Dialog(getActivity());
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(root);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.YELLOW));
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
         return dialog;
+
     }
 
-    public static PlayTrackFragment newInstance(Track track){
+    public static PlayTrackFragment newInstance(String trackId){
         PlayTrackFragment frag = new PlayTrackFragment();
         Bundle args = new Bundle();
-        String trackId = track.id;
-        args.putString(PARCEL_KEY, trackId);
+        args.putString(TRACK_ID_KEY, trackId);
         frag.setArguments(args);
         return frag;
     }
